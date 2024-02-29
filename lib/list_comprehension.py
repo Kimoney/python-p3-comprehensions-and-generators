101ddeb2d15d46a58b4c84461aea6a70c73fd414
John <johntaykimani@gmail.com>
2024-02-29 15:05:03 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # list comprehension :) ... I love one liners :-D
+    evens = [num for num in num_list if (num % 2 == 0)]
+    return evens  
 
 def make_exclamation(sentence_list):
-    pass
+    # list comprehension :) another amazing one liner :_D
+    exclamatory = [sentence + "!" for sentence in sentence_list]
+    return exclamatory
